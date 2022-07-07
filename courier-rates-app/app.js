cd01d8d745289a6a5c1d99e49f5f76a050e1d087
hash997 <alnaash899@gmail.com>
2022-07-07 11:43:55 +0800

    could not fix docker test, added some documentation comments, and added read me

--- courier-rates-app/app.js
+++ courier-rates-app/app.js
@@ -1,31 +1,40 @@
-require("dotenv").config();
 const { isTest } = require("./services/environment.service");
 const express = require("express");
+const routes = require("./routes");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = require("./services/documentations");
 
-// This will be our application entry. We'll setup our server here.
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
 const http = require("http");
 
-// Set up express app
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require("dotenv").config();
+
+// Create the Express application
 const app = express();
 app.use(logger("dev"));
-const v1 = require("./routes/v1");
+
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const jnt = require("./puppeteer/index");
+
+/**
+ * -------------- SWAGGER SETUP ----------------
+ */
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// // Require our routes into the application
-app.use("/", v1);
-
-app.post("/", jnt.getJntRates);
+/**
+ * -------------- ROUTES ----------------
+ */
+app.use("/", routes);
 
 const host = process.env.NODE_HOST || "0.0.0.0";
 const port = parseInt(process.env.NODE_PORT, 10) || 8000;
--- courier-rates-app/config/test.js
+++ courier-rates-app/config/test.js
@@ -1,11 +1,5 @@
 module.exports = {
   dbConfig: {
-    username: "root",
-    password: "password",
-    database: "rates_db_test",
-    host: "mysql_test",
-    port: "4321",
-    dialect: "mysql",
     logging: false,
   },
 };
